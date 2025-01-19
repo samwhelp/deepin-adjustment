a0efd3ff93234caed4408b048e03f6c99b702b2d
samwhelp <samwhelp@users.noreply.github.com>
2025-01-19 20:39:51 +0800

    fix

---
+++ prototype/deepin-25/deepin-config/session-kwin/locale/en_us/Deepin-Dark/README.md
@@ -0,0 +1,82 @@
+
+
+# Deepin Adjustment
+
+* [Source Code](#source-code)
+* [Config File](#config-file)
+* [Howto](#howto)
+
+
+
+
+## Source Code
+
+* deepin-kwin / src / [kwin.kcfg](https://github.com/GXDE-OS/deepin-kwin/blob/5.24/src/kwin.kcfg#L7)
+* deepin-kwin / configures / [deepin-kwinrc](https://github.com/GXDE-OS/deepin-kwin/blob/5.24/configures/deepin-kwinrc#L96-L106)
+
+
+
+
+## Config File
+
+| Config File |
+| ----------- |
+| [~/.config/kactivitymanagerdrc](asset/overlay/etc/skel/.config/kactivitymanagerdrc) |
+| [~/.config/kwinrc](asset/overlay/etc/skel/.config/kwinrc) |
+| [~/.config/kglobalshortcutsrc](asset/overlay/etc/skel/.config/kglobalshortcutsrc) |
+
+
+| Config File |
+| ----------- |
+| [~/.config/deepin](asset/overlay/etc/skel/.config/deepin) |
+
+
+| Config File |
+| ----------- |
+| [~/.config/gtk-3.0/settings.ini](asset/overlay/etc/skel/.config/gtk-3.0/settings.ini) |
+| [~/.config/gtk-3.0/gtk.css](asset/overlay/etc/skel/.config/gtk-3.0/gtk.css) |
+| [~/.config/gtk-3.0/vte-terminal.css](asset/overlay/etc/skel/.config/gtk-3.0/vte-terminal.css) |
+
+
+
+
+## Howto
+
+
+### install
+
+``` sh
+./install.sh
+```
+
+or
+
+``` sh
+make install
+```
+
+
+### package-install
+
+``` sh
+./package-install.sh
+```
+
+or
+
+``` sh
+make package-install
+```
+
+
+### config-install
+
+``` sh
+./config-install.sh
+```
+
+or
+
+``` sh
+make config-install
+```
---
+++ prototype/deepin-25/deepin-config/session-kwin/locale/en_us/Deepin-Dark/config-install.sh
@@ -0,0 +1,240 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+
+
+################################################################################
+### Head: deepin
+##
+
+deepin_config_run_pre () {
+
+	#deepin_service_stop
+
+
+	return 0
+
+}
+
+deepin_config_run_post () {
+
+	#deepin_service_start
+
+
+	return 0
+
+}
+
+deepin_service_stop () {
+
+	systemctl --user stop plasma-kglobalaccel.service
+
+
+	systemctl --user stop plasma-kactivitymanagerd.service
+
+	rm ~/.local/share/kactivitymanagerd/ -rf
+
+
+	systemctl --user stop plasma-plasmashell.service
+
+
+	return 0
+
+}
+
+deepin_service_start () {
+
+	systemctl --user start plasma-kglobalaccel.service
+
+
+	systemctl --user start plasma-plasmashell.service
+
+
+	systemctl --user start plasma-kactivitymanagerd.service
+
+
+	return 0
+
+}
+
+deepin_config_install () {
+
+	echo
+	echo "##"
+	echo "## Config: deepin"
+	echo "##"
+	echo
+
+
+	deepin_config_install_by_dir
+
+	deepin_config_install_by_gsettings
+
+
+	echo
+
+}
+
+deepin_config_install_by_dir () {
+
+
+	echo
+	echo "mkdir -p ${HOME}"
+	mkdir -p "${HOME}"
+
+
+	echo
+	echo "cp -rf ./asset/overlay/etc/skel/. ${HOME}"
+	cp -rf "./asset/overlay/etc/skel/." "${HOME}"
+
+
+}
+
+##
+### Tail: deepin
+################################################################################
+
+
+
+
+################################################################################
+### Head: gsettings
+##
+
+deepin_config_install_by_gsettings () {
+
+	deepin_config_install_by_gsettings_keybind
+
+}
+
+deepin_config_install_by_gsettings_keybind () {
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system launcher "['<Alt>F1']"
+	gsettings set com.deepin.dde.keybinding.system launcher "['<Alt>F1']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system logout "['<Control><Alt>Delete']"
+	gsettings set com.deepin.dde.keybinding.system logout "['<Control><Alt>Delete']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system lock-screen "['<Alt><Shift>l']"
+	gsettings set com.deepin.dde.keybinding.system lock-screen "['<Alt><Shift>l']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system screenshot "['<Alt>Print']"
+	gsettings set com.deepin.dde.keybinding.system screenshot "['<Alt>Print']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system screenshot-delayed "['<Alt><Control>p']"
+	gsettings set com.deepin.dde.keybinding.system screenshot-delayed "['<Alt><Control>p']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system screenshot-fullscreen "['Print']"
+	gsettings set com.deepin.dde.keybinding.system screenshot-fullscreen "['Print']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system screenshot-ocr "['<Control>Print']"
+	gsettings set com.deepin.dde.keybinding.system screenshot-ocr "['<Control>Print']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system screenshot-window "['<Super>Print']"
+	gsettings set com.deepin.dde.keybinding.system screenshot-window "['<Super>Print']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system system-monitor "['<Alt><Control>Escape']"
+	gsettings set com.deepin.dde.keybinding.system system-monitor "['<Alt><Control>Escape']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system deepin-screen-recorder "['<Alt><Control>r']"
+	gsettings set com.deepin.dde.keybinding.system deepin-screen-recorder "['<Alt><Control>r']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system terminal "[]"
+	gsettings set com.deepin.dde.keybinding.system terminal "[]"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system terminal-quake "[]"
+	gsettings set com.deepin.dde.keybinding.system terminal-quake "[]"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system file-manager "[]"
+	gsettings set com.deepin.dde.keybinding.system file-manager "[]"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system notification-center "['<Alt><Control>m']"
+	gsettings set com.deepin.dde.keybinding.system notification-center "['<Alt><Control>m']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system clipboard "['<Alt><Control>n']"
+	gsettings set com.deepin.dde.keybinding.system clipboard "['<Alt><Control>n']"
+
+
+	echo
+	echo gsettings set com.deepin.dde.keybinding.system global-search "['<Alt><Control>f', '<Alt>F2']"
+	gsettings set com.deepin.dde.keybinding.system global-search "['<Alt><Control>f', '<Alt>F2']"
+
+
+	return 0
+}
+
+##
+### Tail: gsettings
+################################################################################
+
+
+
+
+################################################################################
+### Head: config_install
+##
+
+main_config_install () {
+
+	deepin_config_run_pre
+
+	deepin_config_install
+
+	deepin_config_run_post
+
+}
+
+##
+### Tail: config_install
+################################################################################
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	main_config_install
+
+}
+
+##
+## Start
+##
+__main__
+
+##
+### Tail: Main
+################################################################################
